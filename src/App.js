b92a577ccd36a191cbe7167a3c8862b129b95bb7
Yadwendra <yadwendraec.hzp@gmail.com>
2022-09-20 13:09:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,85 @@
-import React from 'react'
-import Navbar from './Component/Navbar.js';
-import News from './Component/News.js';
-import {BrowserRouter as Router,Switch,Route } from "react-router-dom";
-const App =()=>  {
+import React from "react";
+import Navbar from "./Component/Navbar.js";
+import News from "./Component/News.js";
+import { BrowserRouter as Router, Switch, Route, BrowserRouter } from "react-router-dom";
+const App = () => {
   const pageSize = 6;
-  
-  
-    return (
-      <div>
-        <Router> 
-        <Navbar />
-        
-         <Switch>
-          <Route exact path="/"><News key="general" pazeSize={pageSize} country="in" category="general"/></Route>
-          <Route exact  path="/business"><News key="business" pazeSize={pageSize} country="in" category="business"/></Route>
-          <Route exact  path="/entertainment"><News key="entertainment" pazeSize={pageSize} country="in" category="entertainment"/></Route>
-          <Route exact path='/general'><News key="general" pazeSize={pageSize} country='in' category='general'/></Route>
-          <Route exact path='/health'><News key="health" pazeSize={pageSize} country='in' category='health'/></Route>
-          <Route exact path='/science'><News key="science" pazeSize={pageSize} country='in' category='science'/></Route>
-          <Route exact path='/sports'><News key="sports" pazeSize={pageSize} country='in' category='sports'/></Route>
-          <Route exact path='/technology'><News key="technology" pazeSize={pageSize} country='in' category='technology'/></Route>
-        
-          </Switch>
-          
-          
-          
-        
-      </Router>
-  
-      </div>
-    )
-  }
 
-export default App
+  return (
+    <div>
+      <BrowserRouter>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <News
+                key="general"
+                pazeSize={pageSize}
+                country="in"
+                category="general"
+              />
+            </Route>
+            <Route exact path="/business">
+              <News
+                key="business"
+                pazeSize={pageSize}
+                country="in"
+                category="business"
+              />
+            </Route>
+            <Route exact path="/entertainment">
+              <News
+                key="entertainment"
+                pazeSize={pageSize}
+                country="in"
+                category="entertainment"
+              />
+            </Route>
+            <Route exact path="/general">
+              <News
+                key="general"
+                pazeSize={pageSize}
+                country="in"
+                category="general"
+              />
+            </Route>
+            <Route exact path="/health">
+              <News
+                key="health"
+                pazeSize={pageSize}
+                country="in"
+                category="health"
+              />
+            </Route>
+            <Route exact path="/science">
+              <News
+                key="science"
+                pazeSize={pageSize}
+                country="in"
+                category="science"
+              />
+            </Route>
+            <Route exact path="/sports">
+              <News
+                key="sports"
+                pazeSize={pageSize}
+                country="in"
+                category="sports"
+              />
+            </Route>
+            <Route exact path="/technology">
+              <News
+                key="technology"
+                pazeSize={pageSize}
+                country="in"
+                category="technology"
+              />
+            </Route>
+          </Switch>
+        </Router>
+        </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
